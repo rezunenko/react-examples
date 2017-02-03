ae3e8d664f62859547fa78fdb7264916c2b66372
tamerlan88111 <tamerlan88111@yandex.ru>
2017-02-03 16:19:11 +0300

    use connect and react-redux for store

--- samples/09/src/components/App.js
+++ samples/09/src/components/App.js
@@ -1,20 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class App extends React.Component {
+  addTrack() {
+    console.log('AddTrack', this.trackInput.value);
+    this.props.onAddTrack(this.trackInput.value);
+    this.trackInput.value="";
+  }
   render() {
+    console.log(this.props.testStore);
     return (
       <div className="playlist">
         <h1>Hello, {this.props.name}</h1>
-        <input type="text" className="playlist__input" placeholder="Введите название трека"/>
-        <input type="button" className="playlist__btn-add" value="Добавить"/>
+        <input type="text" className="playlist__input" placeholder="Введите название трека" ref={(input) => { this.trackInput = input }}/>
+        <input type="button" className="playlist__btn-add" value="Добавить"  onClick={this.addTrack.bind(this)}/>
         <ul className="playlist__list">
-          <li>asd</li>
-          <li>asdf</li>
-          <li>fff</li>
+          {
+            this.props.testStore.map((track,index) => {
+              return <li key={index}>{track}</li>;
+            })
+          }
         </ul>
       </div>
     )
   }
 }
 
-export default App;
+export default connect(
+  state => ({
+    testStore: state
+  }),
+  dispatch => ({
+    onAddTrack: (trackName) => {
+      dispatch({type: 'ADD_TRACK', payload: trackName})
+    }
+  })
+)(App);
--- samples/09/src/index.js
+++ samples/09/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import './index.scss';
 
 
-import {createStore} from 'redux';
-
-const initialState = []
+const initialState = [
+  'Smells like spirit',
+  'Enter Sandman'
+];
 
 function playList(state = initialState, action) {
   switch(action.type){
@@ -16,31 +20,53 @@ function playList(state = initialState, action) {
   return state;
 }
 
+const store = new createStore(playList);
+
 ReactDOM.render(
-  <App name="Timur"/>,
+  <Provider store={store}>
+    <App name="Timur"/>
+  </Provider>,
   document.getElementById('mount-point')
 );
 
-
-const addTrackBtn = document.querySelector(".playlist__btn-add");
-const trackInput = document.querySelector(".playlist__input");
-const list = document.querySelector(".playlist__list");
-
-addTrackBtn.addEventListener('click', () => {
-    store.dispatch({ type: "ADD_TRACK", payload: trackInput.value });
-});
-
-const store = new createStore(playList);
-
-store.subscribe(() => {
-  trackInput.value = "";
-  list.innerHTML = "";
-  store.getState().forEach(item => {
-    const li = document.createElement("li");
-    li.textContent = item;
-    list.appendChild(li);
-  });
-});
-
+//
+// import {createStore} from 'redux';
+//
+// const initialState = []
+//
+// function playList(state = initialState, action) {
+//   switch(action.type){
+//     case 'ADD_TRACK' : return [... state,action.payload];
+//   }
+//
+//   return state;
+// }
+//
+// ReactDOM.render(
+//   <App name="Timur"/>,
+//   document.getElementById('mount-point')
+// );
+//
+//
+// const addTrackBtn = document.querySelector(".playlist__btn-add");
+// const trackInput = document.querySelector(".playlist__input");
+// const list = document.querySelector(".playlist__list");
+//
+// addTrackBtn.addEventListener('click', () => {
+//     store.dispatch({ type: "ADD_TRACK", payload: trackInput.value });
+// });
+//
+// const store = new createStore(playList);
+//
+// store.subscribe(() => {
+//   trackInput.value = "";
+//   list.innerHTML = "";
+//   store.getState().forEach(item => {
+//     const li = document.createElement("li");
+//     li.textContent = item;
+//     list.appendChild(li);
+//   });
+// });
+//
 
 
